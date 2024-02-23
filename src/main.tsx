4ef7edbf1b72c5ff5e2d6dee8f193cb89dc65d5f
James Suderman <james.suderman@uticanational.com>
2024-02-23 16:10:48 -0600

    First Commit

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from './components/theme-provider.tsx';
+import App from './App.tsx';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ThemeProvider defaultTheme="light" storageKey="dashboard-theme">
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
